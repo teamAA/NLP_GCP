90150b091c79df8b795c7c130d32dd910a6f8fdc
andre <andreasmmadjiah@gmail.com>
2022-02-02 01:33:29 +0700

    edit python version

--- main2.py
+++ main2.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+import dvc.api
 st.set_page_config(layout = "wide")
 
 st.title('Sentiment Analysis')
 st.markdown('The project is to build a model that will determine the tone (neutral, positive, negative) of the tweet text.')
+
+data = pd.read_csv(dvc.api.get_url('data_model/train.csv'))
+total=data.shape[0]
+st.write('Total tweets : ',total)
+
+data2 = data.head()
+st.table(data2)
